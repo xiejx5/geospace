97d59f32ea5e8c668daa1fed3f315a85d4c8a2ba
Cody James <xiejx5@gmail.com>
2023-04-09 22:25:57 +0800

    0.3.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/xiejx5/geospace'
 EMAIL = 'xiejx5@gmail.com'
 AUTHOR = 'Cody James'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.3.0'
+VERSION = '0.3.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
